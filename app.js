74560bf6e18802e1702eb1d0708d81f63110071b
MadhupriyaGundeti <madhupriyareddy1919@gmail.com>
2022-04-25 12:38:57 -0500

    added changes

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport'); 
 var LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }))
 const connectionString =  
 process.env.MONGO_CON 
 mongoose = require('mongoose'); 
 mongoose.connect(connectionString,  
 {useNewUrlParser: true, 
 useUnifiedTopology: true}); 
+//Get the default connection 
+var db = mongoose.connection; 
+ 
+//Bind connection to error event  
+db.on('error', console.error.bind(console, 'MongoDB connection error:')); 
+db.once("open", function(){ 
+  console.log("Connection to DB succeeded")}); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var soupRouter = require('./routes/soup');
@@ -28,6 +48,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -36,6 +63,16 @@ app.use('/soup',soupRouter);
 app.use('/addmods',addmodsRouter);
 app.use('/selector',selectorRouter);
 app.use('/resource', resourceRouter);
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/soup.js
+++ routes/soup.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 const soup_controlers= require('../controllers/soup');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  router.get('/', soup_controlers.soup_view_all_Page)
-  res.render('soup', { title: 'Search Results soup' });
-});
-
+  router.get('/', soup_controlers.soup_view_all_Page);
+/* GET detail soup page */ 
+router.get('/detail', soup_controlers.soup_view_one_Page); 
+/* GET create soup page */ 
+router.get('/create', soup_controlers.soup_create_Page); 
+/* GET create update page */ 
+router.get('/update', soup_controlers.soup_update_Page); 
+/* GET delete soup page */ 
+router.get('/delete', soup_controlers.soup_delete_Page); 
 module.exports = router;
- 
